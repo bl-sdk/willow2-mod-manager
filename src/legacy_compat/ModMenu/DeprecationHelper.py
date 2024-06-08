75bbb1f3bd1d65c592015ffb8af8ce4806088b67
apple1417 <git@apple1417.dev>
2024-06-08 21:32:58 +1200

    deprecation helper aliases

--- src/legacy_compat/ModMenu/DeprecationHelper.py
+++ src/legacy_compat/ModMenu/DeprecationHelper.py
@@ -1,64 +1,34 @@
-import unrealsdk
-import functools
-from typing import Any, Callable, Dict, List, Optional, Set, Tuple
+# ruff: noqa: N802, N803, D102, D103, N999
 
-__all__: Tuple[str, ...] = (
+import functools
+import warnings
+from collections.abc import Callable
+from typing import Any
+
+__all__: tuple[str, ...] = (
     "Deprecated",
     "NameChangeMsg",
     "PrintWarning",
 )
 
-_printed_deprecation_warnings: Set[str] = set()
-
 
 def PrintWarning(msg: str) -> None:
-    """
-    Prints a warning containing the provided message. Will only happen once per message.
-
-    Args:
-        msg: The message to print.
-    """
-    if msg not in _printed_deprecation_warnings:
-        _printed_deprecation_warnings.add(msg)
-        unrealsdk.Log(f"[Warning] {msg}")
+    warnings.warn(msg, DeprecationWarning, stacklevel=3)
 
 
 def NameChangeMsg(old_name: str, new_name: str) -> str:
-    """
-    Helper returning a generic deprecation message for when something's name changed.
-
-    Args:
-        old_name: The deprecated name.
-        new_name: The new name.
-    Returns:
-        The name change deprecation message.
-    """
     return f"Use of '{old_name}' is deprecated, use '{new_name}' instead."
 
 
-def Deprecated(
-    msg: str,
-    func: Optional[Callable[..., Any]] = None
-) -> Callable[..., Any]:
-    """
-    Decorator that prints a deprecation message when it's wrapped function is called.
-
-    Can also be called with the function as an argument.
-
-    Args:
-        msg: The message to print.
-        func: The function to wrap.
-    Returns:
-        The wrapped function
-    """
+def Deprecated(msg: str, func: Callable[..., Any] | None = None) -> Callable[..., Any]:
     def decorator(old_func: Callable[..., Any]) -> Callable[..., Any]:
         @functools.wraps(old_func)
-        def new_func(*args: List[Any], **kwargs: Dict[str, Any]) -> Any:
+        def new_func(*args: Any, **kwargs: Any) -> Any:
             PrintWarning(msg)
             return old_func(*args, **kwargs)
+
         return new_func
 
     if func is None:
         return decorator
-    else:
-        return decorator(func)
+    return decorator(func)
--- src/legacy_compat/ModMenu/__init__.py
+++ src/legacy_compat/ModMenu/__init__.py
@@ -1,11 +1,13 @@
 # ruff: noqa: N802, N803, D102, D103, N999
 
+from . import Options
+from .DeprecationHelper import Deprecated, NameChangeMsg, PrintWarning
 from .ModObjects import EnabledSaveType, Game, ModPriorities, Mods, ModTypes, RegisterMod, SDKMod
 
 __all__: tuple[str, ...] = (
     # "AnyHook",
     # "ClientMethod",
-    # "Deprecated",
+    "Deprecated",
     "EnabledSaveType",
     "Game",
     # "GetOrderedModList",
@@ -20,9 +22,9 @@ __all__: tuple[str, ...] = (
     "ModPriorities",
     "Mods",
     "ModTypes",
-    # "NameChangeMsg",
-    # "Options",
-    # "PrintWarning",
+    "NameChangeMsg",
+    "Options",
+    "PrintWarning",
     # "RegisterHooks",
     "RegisterMod",
     # "RegisterNetworkMethods",
