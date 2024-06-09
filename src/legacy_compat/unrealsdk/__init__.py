2da9811c405674f335e3677d76b3747256828fe9
apple1417 <git@apple1417.dev>
2024-06-09 16:28:45 +1200

    get tuple struct assignment working

--- src/legacy_compat/unrealsdk/__init__.py
+++ src/legacy_compat/unrealsdk/__init__.py
@@ -1,8 +1,13 @@
 # ruff: noqa: N802, N803, D102, D103, N999
 
+import warnings
+from contextlib import suppress
+from functools import wraps
+from typing import Any
+
 from mods_base import ENGINE
 from unrealsdk import find_class, find_object
-from unrealsdk.unreal import UClass, UFunction, UObject
+from unrealsdk.unreal import UClass, UFunction, UObject, UStructProperty, WrappedStruct
 
 __all__: tuple[str, ...] = (
     "FindObject",
@@ -31,3 +36,46 @@ def FindClass(ClassName: str, Lookup: bool = False) -> UClass | None:  # noqa: A
 
 def GetEngine() -> UObject:
     return ENGINE
+
+
+# The legacy SDK had you set structs via a tuple of their values in sequence, we need to convert
+# them to a wrapped struct instance
+_default_object_setattr = UObject.__setattr__
+_default_struct_setattr = WrappedStruct.__setattr__
+
+
+@wraps(UObject.__setattr__)
+def _object_setattr(self: UObject, name: str, value: Any) -> None:
+    if isinstance(value, tuple):
+        with suppress(ValueError):
+            prop = self.Class._find_prop(name)
+            if isinstance(prop, UStructProperty):
+                warnings.warn(
+                    "Setting struct properties using tuples is deprecated. Use"
+                    " unrealsdk.make_tuple(), or WrappedStruct directly.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                value = WrappedStruct(prop.Struct, *value)
+    _default_object_setattr(self, name, value)
+
+
+@wraps(WrappedStruct.__setattr__)
+def _struct_setattr(self: WrappedStruct, name: str, value: Any) -> None:
+    if isinstance(value, tuple):
+        with suppress(ValueError):
+            prop = self._type._find_prop(name)
+            if isinstance(prop, UStructProperty):
+                warnings.warn(
+                    "Setting struct properties using tuples is deprecated. Use"
+                    " unrealsdk.make_tuple(), or WrappedStruct directly.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                value = WrappedStruct(prop.Struct, *value)
+    _default_struct_setattr(self, name, value)
+
+
+# Unfortuantely we need to keep these active the entire time, since the calls happen at runtime
+UObject.__setattr__ = _object_setattr
+WrappedStruct.__setattr__ = _struct_setattr
