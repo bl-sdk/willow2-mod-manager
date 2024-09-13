9fed930be233396fa2a1dfe08c383eaac74e137f
apple1417 <git@apple1417.dev>
2024-09-13 22:46:33 +1200

    port misc ui utils from user feedback

--- src/ui_utils/__init__.py
+++ src/ui_utils/__init__.py
@@ -1,11 +1,16 @@
 from mods_base.mod_list import base_mod
 
+from .chat import show_chat_message
+from .clipboard import clipboard_copy, clipboard_paste
 from .hud_message import show_hud_message
 
 __all__: tuple[str, ...] = (
     "__author__",
     "__version__",
     "__version_info__",
+    "clipboard_copy",
+    "clipboard_paste",
+    "show_chat_message",
     "show_hud_message",
 )
 
