fc5c3f5b8c57b866d2cd8834f931f1dd17691870
apple1417 <git@apple1417.dev>
2025-01-02 17:05:20 +1300

    fixed that some legacy mods would not auto-enable properly

--- src/legacy_compat/ModMenu/ModObjects.py
+++ src/legacy_compat/ModMenu/ModObjects.py
@@ -230,7 +230,16 @@ class _NewMod(Mod):
 
     @property
     def auto_enable(self) -> bool:
-        # Load on main menu is implemented inside LoadModSettings
+        # During the default save/load mod settings function, return true for both types of saving,
+        # to make sure that we add it to the settings file
+        if inspect.stack()[1].function in {
+            "default_load_mod_settings",
+            "default_save_mod_settings",
+        }:
+            return self.legacy_mod.SaveEnabledState != EnabledSaveType.NotSaved
+
+        # At all other times, treat load on main menu as *not* auto enabled, so that we can manually
+        # implement it (inside LoadModSettings), rather than having mods_base load it immediately.
         return self.legacy_mod.SaveEnabledState == EnabledSaveType.LoadWithSettings
 
     @auto_enable.setter
