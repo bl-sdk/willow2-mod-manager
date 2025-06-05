33f76a04db1913e04c3b70e37df36bb5547eccab
juso <justin.sostmann@googlemail.com>
2025-06-05 19:13:54 +0200

    Add new ui functions

--- src/ui_utils/__init__.py
+++ src/ui_utils/__init__.py
@@ -2,13 +2,29 @@ from mods_base.mod_list import base_mod
 
 from .chat import show_chat_message
 from .clipboard import clipboard_copy, clipboard_paste
-from .hud_message import show_hud_message
+from .hud_message import (
+    ERewardPopup,
+    blocking_message_hide,
+    blocking_message_show,
+    contextual_prompt_hide,
+    contextual_prompt_show,
+    display_blocking_message,
+    display_contextual_prompt,
+    display_message,
+    message_hide,
+    message_show,
+    show_big_notification,
+    show_hud_message,
+    show_reward_popup,
+    show_top_center_message,
+)
 from .option_box import OptionBox, OptionBoxButton
 from .reorder_box import ReorderBox
 from .training_box import EBackButtonScreen, TrainingBox
 
 __all__: tuple[str, ...] = (
     "EBackButtonScreen",
+    "ERewardPopup",
     "OptionBox",
     "OptionBoxButton",
     "ReorderBox",
@@ -16,10 +32,22 @@ __all__: tuple[str, ...] = (
     "__author__",
     "__version__",
     "__version_info__",
+    "blocking_message_hide",
+    "blocking_message_show",
     "clipboard_copy",
     "clipboard_paste",
+    "contextual_prompt_hide",
+    "contextual_prompt_show",
+    "display_blocking_message",
+    "display_contextual_prompt",
+    "display_message",
+    "message_hide",
+    "message_show",
+    "show_big_notification",
     "show_chat_message",
     "show_hud_message",
+    "show_reward_popup",
+    "show_top_center_message",
 )
 
 __version_info__: tuple[int, int] = (1, 3)
